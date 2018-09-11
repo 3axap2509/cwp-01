c8849742ac2978740f2e122eecce967410eca75a
3axap2509 <zakayshka2@gmail.com>
2018-09-11 21:25:04 +0300

    перенёс текущие операции в локально создаваемый скрипт

--- task05.js
+++ task05.js
@@ -1,37 +1,47 @@
 var path = require('path');
 var fs = require('fs');
 var folder = process.argv[2];
-fs.readdir(
-    folder, 
-    function(err, items)
-    {
-        if(err)
-        {
-            return console.error(err);
-        }
-        else
-        {
-            items.forEach(function(item)
-            {
-               if(path.extname(item) == '')
-               {
-                fs.readdir(folder + '//' + path.basename(item), function(err, files)
-                {
-                    if(err)
-                    {
-                        return console.error(err);
-                    }
-                    else
-                    {
-                        files.forEach(function(file)
-                        {
-                            console.log(path.basename(item) + '/' +  path.basename(file));
-                        })
-                    }
-                })
-               }
-               console.log(path.basename(item));
-            });
-        }
-    }
-    )
+var newScript = folder + '//summary.js';
+
+
+
+fs.open(newScript, "w+", 0644, function(err, file_handler)
+{
+
+})
+fs.writeFile(newScript, "var path = require('path');var fs = require('fs');var f = process.argv[1];for(var i = 0; i < 10; i++){f = f.substring(0, f.length-1);}fs.readdir(f,function(err, items){if(err){return console.error(err);}else{items.forEach(function(item){if(path.extname(item) == ''){fs.readdir(f + '//' + path.basename(item), function(err, files){if(err){return console.error(err);}else{files.forEach(function(file){console.log(path.basename(item) + '/' +  path.basename(file));})}})}console.log(path.basename(item));});}})", function(eror){if(eror){return console.err(err)}});
+
+// fs.readdir(
+//     folder, 
+//     function(err, items)
+//     {
+//         if(err)
+//         {
+//             return console.error(err);
+//         }
+//         else
+//         {
+//             items.forEach(function(item)
+//             {
+//                if(path.extname(item) == '')
+//                {
+//                 fs.readdir(folder + '//' + path.basename(item), function(err, files)
+//                 {
+//                     if(err)
+//                     {
+//                         return console.error(err);
+//                     }
+//                     else
+//                     {
+//                         files.forEach(function(file)
+//                         {
+//                             console.log(path.basename(item) + '/' +  path.basename(file));
+//                         })
+//                     }
+//                 })
+//                }
+//                console.log(path.basename(item));
+//             });
+//         }
+//     }
+//     )
